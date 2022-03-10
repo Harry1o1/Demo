bd9b9262dcd633589b4baed38d6a0be155f58ce6
ajju40959@gmail.com <ajju40959@gmail.com>
2022-03-10 17:37:37 +0600

    Ham mo maro ham ko maro ham ko zinda mat soro salo

--- nextjs/components/navbar.js
+++ nextjs/components/navbar.js
@@ -1,9 +1,9 @@
 import React from 'react'
 // import {Mb_NavbarTop,Mb_BottomBar,Pc_NavbarTop,Pc_SideBar} from './Navbars';
-import Mb_NavbarTop from './Navbars/Mb_NavbarTop';
-import Mb_BottomBar from './Navbars/Mb_BottomBar';
-import Pc_NavbarTop from './Navbars/Pc_NavbarTop';
-import Pc_SideBar from './Navbars/Pc_SideBar';
+import Mb_NavbarTop from './Navbars/mb-navbartop';
+import Mb_BottomBar from './Navbars/mb-bottombar';
+import Pc_NavbarTop from './Navbars/pc-navbartop';
+import Pc_SideBar from './Navbars/pc-sidebar';
 
 
 
