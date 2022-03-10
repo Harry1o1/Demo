874d6e7b27a69a2f0247fe169e45836e8b2c2fbf
ajju40959@gmail.com <ajju40959@gmail.com>
2022-03-10 17:52:14 +0600

    hi

--- nextjs/components/navbar.js
+++ nextjs/components/navbar.js
@@ -1,9 +1,9 @@
 import React from 'react'
 // import {Mb_NavbarTop,Mb_BottomBar,Pc_NavbarTop,Pc_SideBar} from './Navbars';
-import Mb_NavbarTop from './Navbars/mbnavbartop';
-import Mb_BottomBar from './Navbars/mbbottombar';
-import Pc_NavbarTop from './Navbars/pcnavbartop';
-import Pc_SideBar from './Navbars/pcsidebar';
+import Mb1 from './Navbars/mbnavbartop';
+import Mb2 from './Navbars/mbbottombar';
+import Pc1 from './Navbars/pcnavbartop';
+import Pc2 from './Navbars/pcsidebar';
 
 
 
