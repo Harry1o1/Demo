849139c0d76d63222c39bf910bf502c5d4b624d2
ajju40959@gmail.com <ajju40959@gmail.com>
2022-03-10 19:56:16 +0600

    Hello bro ka haal chaal

--- nextjs/components/Navbars/pcsidebar.js
+++ nextjs/components/Navbars/pcsidebar.js
@@ -16,39 +16,272 @@ function Pc2() {
                       <input type="text" placeholder="Search...">
                   </li> */}
                   
-            <ul className="menu-links">
-              <li className="nav-link">
-                <a href="#">
-                  {/* <ion-icon class="icon" name="albums-outline" id="photo-search"></ion-icon> */}
-                  <ion-icon name="menu-outline" className="icon" />
-                  <span className="text nav-text">Dashboard</span>
-                </a>
-              </li>
-              <li className="nav-link">
-                <a href="#">
-                  {/* <ion-icon class="icon" name="albums-outline" id="photo-search"></ion-icon> */}
-                  <span className="text nav-text">Wallets</span>
-                </a>
-              </li>
-              <li className="nav-link">
-                <a href="#">
-                  {/* <ion-icon class="icon" name="albums-outline" id="photo-search"></ion-icon> */}
-                  <span className="text nav-text">Wallets</span>
-                </a>
-              </li>
-              <li className="nav-link">
-                <a href="#">
-                  {/* <ion-icon class="icon" name="albums-outline" id="photo-search"></ion-icon> */}
-                  <span className="text nav-text">Wallets</span>
-                </a>
-              </li>
-              <li className="nav-link">
-                <a href="#">
-                  {/* <ion-icon class="icon" name="albums-outline" id="photo-search"></ion-icon> */}
-                  <span className="text nav-text">Wallets</span>
-                </a>
-              </li>
-            </ul>
+<ul className="menu-links">
+  <li className="nav-link">
+    <a href="#">
+      <i className="bx bx-home-alt icon" />
+      <span className="text nav-text">Dashboard</span>
+    </a>
+  </li>
+  <li className="nav-link">
+    <a href="#">
+      <i className="bx bx-bar-chart-alt-2 icon" />
+      <span className="text nav-text">Revenue</span>
+    </a>
+  </li>
+  <li className="nav-link">
+    <a href="#">
+      <i className="bx bx-bell icon" />
+      <span className="text nav-text">Notifications</span>
+    </a>
+  </li>
+  <li className="nav-link">
+    <a href="#">
+      <i className="bx bx-pie-chart-alt icon" />
+      <span className="text nav-text">Analytics</span>
+    </a>
+  </li>
+  <li className="nav-link">
+    <a href="#">
+      <i className="bx bx-heart icon" />
+      <span className="text nav-text">Likes</span>
+    </a>
+  </li>
+  <li className="nav-link">
+    <a href="#">
+      <i className="bx bx-wallet icon" />
+      <span className="text nav-text">Wallets</span>
+    </a>
+  </li>
+  <li className="nav-link">
+    <a href="#">
+      <i className="bx bx-wallet icon" />
+      <span className="text nav-text">Wallets</span>
+    </a>
+  </li>
+  <li className="nav-link">
+    <a href="#">
+      <i className="bx bx-wallet icon" />
+      <span className="text nav-text">Wallets</span>
+    </a>
+  </li>
+  <li className="nav-link">
+    <a href="#">
+      <i className="bx bx-wallet icon" />
+      <span className="text nav-text">Wallets</span>
+    </a>
+  </li>
+  <li className="nav-link">
+    <a href="#">
+      <i className="bx bx-wallet icon" />
+      <span className="text nav-text">Wallets</span>
+    </a>
+  </li>
+  <li className="nav-link">
+    <a href="#">
+      <i className="bx bx-wallet icon" />
+      <span className="text nav-text">Wallets</span>
+    </a>
+  </li>
+  <li className="nav-link">
+    <a href="#">
+      <i className="bx bx-wallet icon" />
+      <span className="text nav-text">Wallets</span>
+    </a>
+  </li>
+  <li className="nav-link">
+    <a href="#">
+      <i className="bx bx-wallet icon" />
+      <span className="text nav-text">Wallets</span>
+    </a>
+  </li>
+  <li className="nav-link">
+    <a href="#">
+      <i className="bx bx-wallet icon" />
+      <span className="text nav-text">Wallets</span>
+    </a>
+  </li>
+  <li className="nav-link">
+    <a href="#">
+      <i className="bx bx-wallet icon" />
+      <span className="text nav-text">Wallets</span>
+    </a>
+  </li>
+  <li className="nav-link">
+    <a href="#">
+      <i className="bx bx-wallet icon" />
+      <span className="text nav-text">Wallets</span>
+    </a>
+  </li>
+  <li className="nav-link">
+    <a href="#">
+      <i className="bx bx-wallet icon" />
+      <span className="text nav-text">Wallets</span>
+    </a>
+  </li>
+  <li className="nav-link">
+    <a href="#">
+      <i className="bx bx-wallet icon" />
+      <span className="text nav-text">Wallets</span>
+    </a>
+  </li>
+  <li className="nav-link">
+    <a href="#">
+      <i className="bx bx-wallet icon" />
+      <span className="text nav-text">Wallets</span>
+    </a>
+  </li>
+  <li className="nav-link">
+    <a href="#">
+      <i className="bx bx-wallet icon" />
+      <span className="text nav-text">Wallets</span>
+    </a>
+  </li>
+  <li className="nav-link">
+    <a href="#">
+      <i className="bx bx-wallet icon" />
+      <span className="text nav-text">Wallets</span>
+    </a>
+  </li>
+  <li className="nav-link">
+    <a href="#">
+      <i className="bx bx-wallet icon" />
+      <span className="text nav-text">Wallets</span>
+    </a>
+  </li>
+  <li className="nav-link">
+    <a href="#">
+      <i className="bx bx-wallet icon" />
+      <span className="text nav-text">Wallets</span>
+    </a>
+  </li>
+  <li className="nav-link">
+    <a href="#">
+      <i className="bx bx-wallet icon" />
+      <span className="text nav-text">Wallets</span>
+    </a>
+  </li>
+  <li className="nav-link">
+    <a href="#">
+      <i className="bx bx-wallet icon" />
+      <span className="text nav-text">Wallets</span>
+    </a>
+  </li>
+  <li className="nav-link">
+    <a href="#">
+      <i className="bx bx-wallet icon" />
+      <span className="text nav-text">Wallets</span>
+    </a>
+  </li>
+  <li className="nav-link">
+    <a href="#">
+      <i className="bx bx-wallet icon" />
+      <span className="text nav-text">Wallets</span>
+    </a>
+  </li>
+  <li className="nav-link">
+    <a href="#">
+      <i className="bx bx-wallet icon" />
+      <span className="text nav-text">Wallets</span>
+    </a>
+  </li>
+  <li className="nav-link">
+    <a href="#">
+      <i className="bx bx-wallet icon" />
+      <span className="text nav-text">Wallets</span>
+    </a>
+  </li>
+  <li className="nav-link">
+    <a href="#">
+      <i className="bx bx-wallet icon" />
+      <span className="text nav-text">Wallets</span>
+    </a>
+  </li>
+  <li className="nav-link">
+    <a href="#">
+      <i className="bx bx-wallet icon" />
+      <span className="text nav-text">Wallets</span>
+    </a>
+  </li>
+  <li className="nav-link">
+    <a href="#">
+      <i className="bx bx-wallet icon" />
+      <span className="text nav-text">Wallets</span>
+    </a>
+  </li>
+  <li className="nav-link">
+    <a href="#">
+      <i className="bx bx-wallet icon" />
+      <span className="text nav-text">Wallets</span>
+    </a>
+  </li>
+  <li className="nav-link">
+    <a href="#">
+      <i className="bx bx-wallet icon" />
+      <span className="text nav-text">Wallets</span>
+    </a>
+  </li>
+  <li className="nav-link">
+    <a href="#">
+      <i className="bx bx-wallet icon" />
+      <span className="text nav-text">Wallets</span>
+    </a>
+  </li>
+  <li className="nav-link">
+    <a href="#">
+      <i className="bx bx-wallet icon" />
+      <span className="text nav-text">Wallets</span>
+    </a>
+  </li>
+  <li className="nav-link">
+    <a href="#">
+      <i className="bx bx-wallet icon" />
+      <span className="text nav-text">Wallets</span>
+    </a>
+  </li>
+  <li className="nav-link">
+    <a href="#">
+      <i className="bx bx-wallet icon" />
+      <span className="text nav-text">Wallets</span>
+    </a>
+  </li>
+  <li className="nav-link">
+    <a href="#">
+      <i className="bx bx-wallet icon" />
+      <span className="text nav-text">Wallets</span>
+    </a>
+  </li>
+  <li className="nav-link">
+    <a href="#">
+      <i className="bx bx-wallet icon" />
+      <span className="text nav-text">Wallets</span>
+    </a>
+  </li>
+  <li className="nav-link">
+    <a href="#">
+      <i className="bx bx-wallet icon" />
+      <span className="text nav-text">Wallets</span>
+    </a>
+  </li>
+  <li className="nav-link">
+    <a href="#">
+      <i className="bx bx-wallet icon" />
+      <span className="text nav-text">Wallets</span>
+    </a>
+  </li>
+  <li className="nav-link">
+    <a href="#">
+      <i className="bx bx-wallet icon" />
+      <span className="text nav-text">Wallets</span>
+    </a>
+  </li>
+  <li className="nav-link">
+    <a href="#">
+      <i className="bx bx-wallet icon" />
+      <span className="text nav-text">Wallets</span>
+    </a>
+  </li>
+</ul>                  
             </div>
             <div className="bottom-content">
               <li className>
