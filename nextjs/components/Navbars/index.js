1680e54cc9a66773ce0e9fcb9f60ac8ef33996eb
ajju40959@gmail.com <ajju40959@gmail.com>
2022-03-10 17:18:48 +0600

    Same silly mistake 😂😂

--- nextjs/components/Navbars/index.js
+++ nextjs/components/Navbars/index.js
@@ -1,8 +1,8 @@
 
-export { default as Mb_BottomBar } from 'mb-bottombar';
-export { default as Mb_NavbarTop } from 'mb-navbartop';
-export { default as Pc_NavbarTop } from 'pc-navbartop';
-export { default as Pc_SideBar } from 'pc-sidebar';
+export { default as Mb_BottomBar } from './mb-bottombar';
+export { default as Mb_NavbarTop } from './mb-navbartop';
+export { default as Pc_NavbarTop } from './pc-navbartop';
+export { default as Pc_SideBar } from './pc-sidebar';
 
 
 
