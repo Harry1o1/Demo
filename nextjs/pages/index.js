5584f7ab559bc36ad7fc89d56b410719f658e6e8
ajju40959@gmail.com <ajju40959@gmail.com>
2022-03-13 01:27:32 +0600

    op

--- nextjs/pages/index.js
+++ nextjs/pages/index.js
@@ -52,7 +52,7 @@ export default function Home1o1() {
         <link href="https://cdn.jsdelivr.net/gh/Harry1o1/Demo/nextjs//education-website/assets/css/style1o1.css" rel="stylesheet" />
         <link href="https://cdn.jsdelivr.net/gh/Harry1o1/Demo/nextjs//education-website/assets/css/variable.css" rel="stylesheet" />
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" />
         
         
         
