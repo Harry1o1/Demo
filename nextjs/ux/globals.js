ed1a3590a23a28180313bf473802d63d9916ac53
ajju40959@gmail.com <ajju40959@gmail.com>
2022-03-13 22:24:01 +0600

    Hello 🤗🤗🤗 1 day left

--- nextjs/ux/globals.js
+++ nextjs/ux/globals.js
@@ -18,7 +18,10 @@ let html = d('html'),
     toggle = d('#pntcg1'),
     modeSwitch = d('.toggle-switch'),
     modeText = d('.mode-text'),
-    op = 2005
+    op = 2005,
+    drop_down = d('.drop-down'),
+    drop_downon = d('.cross1'),
+    drop_downoff = d('.cross2')
     
     
 
@@ -48,8 +51,26 @@ let html = d('html'),
 
 
 
-if (toggle != null && sidebar != null && modeSwitch != null && modeText != null && op != null) {
+if (toggle != null && sidebar != null && modeSwitch != null && modeText != null && op != null && drop_downon !=  null && drop_down != null) {
     console.log('toggle');
+    
+    drop_down.addEventListener('click', ()=> {
+        // drop_downon.style.display=block;
+        // drop_downoff.style.display=none;
+        console.log('op');
+        
+        
+        
+    })
+    drop_down.addEventListener('click', ()=> {
+        console.log('op');
+        drop_downon.style.display='none';
+        drop_downoff.style.display='block';
+        
+
+    })
+    
+    
 }
 else{
     console.log('no');
