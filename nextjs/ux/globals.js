4832b812a79da30da79b1653943978308b620343
ajju40959@gmail.com <ajju40959@gmail.com>
2022-03-15 01:01:53 +0600

    Felling very sleppi

--- nextjs/ux/globals.js
+++ nextjs/ux/globals.js
@@ -10,6 +10,9 @@ import { Robin } from './src/toogle/robin.js';
 const d = (e)=> {
     return document.querySelector(e);
 }
+const da = (e)=> {
+    return document.querySelectorAll(e);
+}
 
 
 let html = d('html'),
@@ -21,7 +24,11 @@ let html = d('html'),
     op = 2005,
     drop_down = d('.drop-down'),
     drop_downon = d('.cross1'),
-    drop_downoff = d('.cross2')
+    drop_downoff = d('.cross2'),
+    arrows = da('.arrows'),
+    l_arrow=d('#left-arrow'),
+    r_arrow=d('#right-arrow'),
+    ptli=d('.ptli')
     
     
 
@@ -40,9 +47,9 @@ let html = d('html'),
         body.classList.toggle("dark");
         
         if(body.classList.contains("dark")){
-            modeText.innerText = "Light mode";
+            modeText.innerText = "Buyer Mode";
         }else{
-            modeText.innerText = "Dark mode";
+            modeText.innerText = "Seller Mode";
             
         }
     });
@@ -51,9 +58,9 @@ let html = d('html'),
 
 
 
-if (toggle != null && sidebar != null && modeSwitch != null && modeText != null && op != null && drop_downon !=  null && drop_down != null) {
+if (toggle != null && sidebar != null && modeSwitch != null && modeText != null && op != null && drop_downon !=  null && drop_down != null && arrows != null) {
     console.log('toggle');
-    
+    // let index = 0; 
     // drop_down.addEventListener('click', ()=> {
     //     // drop_downon.style.display=block;
     //     // drop_downoff.style.display=none;
@@ -69,6 +76,59 @@ if (toggle != null && sidebar != null && modeSwitch != null && modeText != null 
         
 
     // })
+    
+    
+    
+    // arrows.dblclick = () => {
+    //     console.log('op');
+    // }
+    window.addEventListener('dblclick', ()=>{
+        // console.log('op');
+    })
+    l_arrow.addEventListener('click',()=>{
+        
+        // let index = index !== 0 ? index-1 : 2;
+        // let indexf = () => {
+        //     let index = 1;
+        //     index++;
+            
+        //     console.log(index);
+        //     return index;
+        // }
+        // indexf();
+        // console.log('l');
+        // console.log(indexf);
+        // indexf();
+        // ptli.style.translateX='10%';
+        // ptli.style.backgroundr='green';
+        // ptli.style.display='none';
+        
+        body.style.background='red';
+        body.style.transform='translateY(-40px)';
+        ptli.style.transform='translateX(-150px)';
+        
+        // arrows.style.background='red';
+        
+        // console.log(ptli);
+        // ptli.onfocus=()=>{
+        //     console.log('op');
+        // 
+        // console.log('op');
+        
+    })
+    r_arrow.addEventListener('click',()=>{
+        // let index = 0;
+        // let index = index !== 0 ? index+1 : 0;
+        
+        // console.log(index);
+        body.style.background='yellow';
+        body.style.transform='translateY(40px)';
+        ptli.style.transform='translateX(90%)';
+        
+        console.log('r');
+        ptli.style.translateX='-70px';
+        
+    })
     
     
 }
