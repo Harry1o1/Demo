2b6538f18ed1fae722e99f6d5b60d89f3ac61895
ajju40959@gmail.com <ajju40959@gmail.com>
2022-03-22 02:30:39 +0600

    op2

--- django1o2/django1o2/settings.py
+++ django1o2/django1o2/settings.py
@@ -73,12 +73,22 @@ WSGI_APPLICATION = 'django1o2.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+DATABASES = {  
+    'default': {  
+        'ENGINE': 'django.db.backends.mysql',  
+        'NAME': 'Hi',  
+        'USER': 'root',  
+        'PASSWORD': '',  
+        'HOST': '127.0.0.1',  
+        'PORT': '3306',  
+    }  
+}  
 
 
 # Password validation
