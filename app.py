8c665e0038e040f539c244d90a8fe7d0842177d9
ajju40959@gmail.com <ajju40959@gmail.com>
2022-03-21 10:57:52 +0600

    hi

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import pyttsx3
 Assistant=pyttsx3.init("sapi5")
 voices=Assistant.getProperty("voices")
 print("voices")
-Assistant.setProperty("voices",voices.[0].id)
+Assistant.setProperty("voices",voices[0].id)
 
 
 
